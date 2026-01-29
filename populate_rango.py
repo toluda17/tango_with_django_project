1d19f02a0b8898761547052543ccb90c22b09a76
Toluwani David-Ashiru <2925102D@student.gla.ac.uk>
2026-01-29 13:52:09 +0000

    Test 5 passed and Exercises done

--- populate_rango.py
+++ populate_rango.py
@@ -1,7 +1,6 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'tango_with_django_project.settings')
-
 import django
 django.setup()
 from rango.models import Category, Page
@@ -14,7 +13,7 @@ def populate():
          'url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/'} ]
-
+    
     django_pages = [
         {'title':'Official Django Tutorial',
          'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
@@ -22,22 +21,22 @@ def populate():
          'url':'http://www.djangorocks.com/'},
         {'title':'How to Tango with Django',
          'url':'http://www.tangowithdjango.com/'} ]
-
+    
     other_pages = [
         {'title':'Bottle',
          'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask',
          'url':'http://flask.pocoo.org'} ]
-
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
-
+    
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
+    
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
-
+    
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
@@ -49,8 +48,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
